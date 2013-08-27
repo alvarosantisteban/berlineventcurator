b210f99b0206d37270511d3ea442788c007eb02f
Alvaro <alvarosantisteban@gmail.com>
2013-08-27 19:17:20 +0200

    Change First Time Activity to adjust to new implementation.

--- src/com/alvarosantisteban/berlincurator/FirstTimeActivity.java
+++ src/com/alvarosantisteban/berlincurator/FirstTimeActivity.java
@@ -9,8 +9,6 @@ import java.util.Set;
 
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
-import com.j256.ormlite.table.TableUtils;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -43,13 +41,6 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	//public static List<List<Event>> events = (ArrayList)new ArrayList <ArrayList<Event>>();
 	
 	String tag = "First time Activity";
-	/**
-	 * The map with the events.
-	 * The key is the date and the value its corresponding list of events.
-	 *
-	public static Map<String, List<Event>> events = (Map<String, List<Event>>)(Map<String,?>) new HashMap <String, ArrayList<Event>>();
-	public static List<Event> eventsList;
-	*/
 	
 	public static int actionBarHeight;
 	
@@ -67,26 +58,14 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	 * The total set of webs where the events can be extracted
 	 */
 	public static String[] websNames = {IHeartBerlinEventLoader.WEBSITE_NAME, 
-									ArtParasitesEventLoader.WEBSITE_NAME, 
-									MetalConcertsEventLoader.WEBSITE_NAME, 
-									WhiteTrashEventLoader.WEBSITE_NAME, 
-									KoepiEventLoader.WEBSITE_NAME, 
-									GothDatumEventLoader.WEBSITE_NAME, 
-									StressFaktorEventLoader.WEBSITE_NAME, 
-									IndexEventLoader.WEBSITE_NAME};
+										ArtParasitesEventLoader.WEBSITE_NAME, 
+										MetalConcertsEventLoader.WEBSITE_NAME, 
+										WhiteTrashEventLoader.WEBSITE_NAME, 
+										KoepiEventLoader.WEBSITE_NAME, 
+										GothDatumEventLoader.WEBSITE_NAME, 
+										StressFaktorEventLoader.WEBSITE_NAME, 
+										IndexEventLoader.WEBSITE_NAME};
 
-
-	/**
-	 * The set of urls from where the html will be downloaded
-	 */
-   	public static String[] stringUrls = {IHeartBerlinEventLoader.WEBSITE_URL,
-				   			ArtParasitesEventLoader.WEBSITE_URL,
-				   			MetalConcertsEventLoader.WEBSITE_URL,
-				   			WhiteTrashEventLoader.WEBSITE_URL,
-				   			KoepiEventLoader.WEBSITE_URL,
-				   			GothDatumEventLoader.WEBSITE_URL,
-				   			StressFaktorEventLoader.WEBSITE_URL,
-				   			IndexEventLoader.WEBSITE_URL};
    	/**
    	 * The progress bar for downloading and extracting the events
    	 */
@@ -97,56 +76,28 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	 */
     Button loadButton;
     
-    /**
-	 * The Database Helper that helps dealing with the db easily
-	 */
-	//private DatabaseHelper databaseHelper = null;
+    private static Toast toast;
 	
 	/**
 	 * Loads the elements from the resources
 	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_first_time);
 		System.out.println("--------------- BEGIN ------------");
-		
+		// Set the context
 		context = this;
+		setContentView(R.layout.activity_first_time);
 		loadButton = (Button) findViewById(R.id.loadButton);
 		loadProgressBar = (ProgressBar)findViewById(R.id.progressLoadHtml);	
+		toast = Toast.makeText(this, "", Toast.LENGTH_LONG);
+		
 		// Get the default shared preferences
 		sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-		
 		
 		// This is suppose to be used to clear the data from shared preferences
 		//Editor editor = sharedPref.edit();
 		//editor.clear();
 		//editor.commit();
-		 
-		
-		// Get the sites are meant to be shown
-		Set<String> set = sharedPref.getStringSet(SettingsFragment.KEY_PREF_MULTILIST_SITES, new HashSet<String>(Arrays.asList(websNames)));
-		websNames = set.toArray(new String[0]);
-		
-		Editor editor = sharedPref.edit();
-		editor.putBoolean("isFirstTimeApp", false);
-		editor.commit();
-		
-
-		/*
-		 * To check that the websites are there
-		 * */
-		System.out.println();
-		for(int i=0;i<websNames.length;i++){
-			System.out.print(websNames[i] + " / ");
-		}
-		System.out.println();
-		
-		// Get the height of the action bar
-		TypedValue tv = new TypedValue();
-		context.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true);
-		actionBarHeight = getResources().getDimensionPixelSize(tv.resourceId);
-		
-		//eventsList = new ArrayList<Event>();
 		
 		// CODIGO PARA BORRAR DE LA BASE DE DATOS
 		// The first one creates problems, the second one takes longer
@@ -158,8 +109,33 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 			System.out.println("PETATTTSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
 			e.printStackTrace();
 		}
-		*/
+		*/		
+		 
 		
+		// Get the sites are meant to be shown
+		Set<String> setOfWebsites = sharedPref.getStringSet(SettingsFragment.KEY_PREF_MULTILIST_SITES, new HashSet<String>(Arrays.asList(context.getResources().getStringArray(R.array.default_sites_array))));
+		websNames = setOfWebsites.toArray(new String[0]);
+		
+		// Mark that the App has been used
+		Editor editor = sharedPref.edit();
+		editor.putBoolean("isFirstTimeApp", false);
+		editor.commit();
+		
+		/*
+		 * To check that the websites are there
+		*/
+		System.out.println();
+		for(int i=0;i<websNames.length;i++){
+			System.out.print(websNames[i] + " / ");
+		}
+		System.out.println();
+		
+		// Get the height of the action bar
+		TypedValue tv = new TypedValue();
+		context.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true);
+		actionBarHeight = getResources().getDimensionPixelSize(tv.resourceId);
+
+		// Listener for the load button
 		loadButton.setOnClickListener(new OnClickListener() {
 			
 			/**
@@ -179,7 +155,7 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 			    if (networkInfo != null && networkInfo.isConnected()) {
 					DownloadWebpageTask download = new DownloadWebpageTask();
 					// Execute the asyncronous task of downloading the websites
-					download.execute(stringUrls);
+					download.execute(websNames);
 			    } else {
 			    	// Inform the user that there is no network connection available
 			    	toast = Toast.makeText(getBaseContext(), "No network connection available.", Toast.LENGTH_LONG);
@@ -220,21 +196,7 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	public void onDestroy() {
 	    super.onDestroy();
 	    System.out.println(tag + "In the onDestroy() event");
-	    /*
-	    if (databaseHelper != null) {
-	        OpenHelperManager.releaseHelper();
-	        databaseHelper = null;
-	    }*/
 	}
-	
-	/*
-	private DatabaseHelper getHelper() {
-	    if (databaseHelper == null) {
-	        databaseHelper =
-	            OpenHelperManager.getHelper(this, DatabaseHelper.class);
-	    }
-	    return databaseHelper;
-	}*/
 
 	
 	/**
@@ -244,7 +206,6 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.first_time, menu);
-		//getMenuInflater().inflate(R.menu.preferences, menu);
 		return true;
 	}
 	
@@ -261,13 +222,19 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
         return true;
     }
 
+	private void displayToast(final String message) {
+	    //toast.cancel();
+	    toast.setText(message); 
+	    toast.setGravity(Gravity.TOP, 0, FirstTimeActivity.actionBarHeight);
+	    toast.show();
+	}
     
     /** 
      * Uses AsyncTask to create a task away from the main UI thread. 
      * This task takes the creates several HttpUrlConnection to download the html from different websites. 
      * Afterwards, the several lists of Events are created and the execution goes to the Date Activity.
     */
-	public class DownloadWebpageTask extends AsyncTask<String, String, Map<String,List<Event>>> {
+	public class DownloadWebpageTask extends AsyncTask<String, String, Integer> {
 		
 		/**
 		 * Makes the progressBar visible
@@ -275,98 +242,128 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 		protected void onPreExecute(){
 	    	System.out.println("onPreExecute");
 	    	loadProgressBar.setVisibility(View.VISIBLE);
+	    	// Inform the user
+	    	publishProgress("Start", "");
 		}
 		
 		/**
 		 * Downloads the htmls and creates the lists of Events. 
 		 * Detects any possible problem during the download of the website or the extraction of events.
+		 * @return 
 		 * 
 		 */
-		protected Map<String,List<Event>> doInBackground(String... urls) { 	
-			// Remove all the entries from the map
-			//events.clear();
+		protected Integer doInBackground(String... urls) { 
+			int numOfNewEvents = 0;
 			// Load the events from the selected websites
-			for (int i=0; i<websNames.length; i++){
-				List<Event> event = null;
-				if (websNames[i].equals("I Heart Berlin")){
+			for (int i=0; i<urls.length; i++){
+				List<Event> eventsFromAWebsite = null;
+				if (urls[i].equals("I Heart Berlin")){
 					System.out.println("Ihearberlin dentro");
-					event = EventLoaderFactory.newIHeartBerlinEventLoader().load(context);
-				}else if(websNames[i].equals("Berlin Art Parasites")){
+					eventsFromAWebsite = EventLoaderFactory.newIHeartBerlinEventLoader().load(context);
+				}else if(urls[i].equals("Berlin Art Parasites")){
 					System.out.println("artParasites dentro");
-					event = EventLoaderFactory.newArtParasitesEventLoader().load(context);
-				}else if(websNames[i].equals("Metal Concerts")){
+					eventsFromAWebsite = EventLoaderFactory.newArtParasitesEventLoader().load(context);
+				}else if(urls[i].equals("Metal Concerts")){
 					System.out.println("metalConcerts dentro");
-					event = EventLoaderFactory.newMetalConcertsEventLoader().load(context);
-				}else if(websNames[i].equals("White Trash")){
+					eventsFromAWebsite = EventLoaderFactory.newMetalConcertsEventLoader().load(context);
+				}else if(urls[i].equals("White Trash")){
 					System.out.println("whitetrash dentro");
-					event = EventLoaderFactory.newWhiteTrashEventLoader().load(context);
-				}else if(websNames[i].equals("Köpi's events")){
+					eventsFromAWebsite = EventLoaderFactory.newWhiteTrashEventLoader().load(context);
+				}else if(urls[i].equals("Köpi's events")){
 					System.out.println("koepi dentro");
-					event = EventLoaderFactory.newKoepiEventLoader().load(context);
-				}else if(websNames[i].equals("Goth Datum")){
+					eventsFromAWebsite = EventLoaderFactory.newKoepiEventLoader().load(context);
+				}else if(urls[i].equals("Goth Datum")){
 					System.out.println("goth dentro");
-					event = EventLoaderFactory.newGothDatumEventLoader().load(context);
-				}else if(websNames[i].equals("Stress Faktor")){
+					eventsFromAWebsite = EventLoaderFactory.newGothDatumEventLoader().load(context);
+				}else if(urls[i].equals("Stress Faktor")){
 					System.out.println("Stresssssss faktor dentro");
-					event = EventLoaderFactory.newStressFaktorEventLoader().load(context);
-				}else if(websNames[i].equals("Index")){
+					eventsFromAWebsite = EventLoaderFactory.newStressFaktorEventLoader().load(context);
+				}else if(urls[i].equals("Index")){
 					System.out.println("Index dentro");
-					event = EventLoaderFactory.newIndexEventLoader().load(context);
+					eventsFromAWebsite = EventLoaderFactory.newIndexEventLoader().load(context);
 				}else{
 					return null;
 				}
 				// If there was a problem loading the events we tell the user
-				if (event == null){
-					System.out.println("Event is null");
-					publishProgress("Exception", websNames[i]);		
+				if (eventsFromAWebsite == null){
+					// Distinguish the situation where the event is null because the Berlin Art Parasites website is not checked
+					if(!(urls[i].equals("Berlin Art Parasites") && !ArtParasitesEventLoader.isBerlinWeekend())){
+						System.out.println("Event is null");
+						publishProgress("Exception", urls[i]);	
+					}
 				}else{
-					// If not, we store its events
-					//events.put(websNames[i], event);
-					//organizeByDays(event);
-					//events.put(event.get(0).getDay(), event);
-					for (int j = 0; j < event.size(); j++) {
-						addEventToDB(event.get(j));
+					// Add the events from this website to the DB
+					for (int j = 0; j < eventsFromAWebsite.size(); j++) {
+						if(addEventToDB(eventsFromAWebsite.get(j))){
+							numOfNewEvents++;
+						}
 					}
 				}
 			}
-			
-			//return events;
-			return null;
-		} 
+			// Inform the user
+			publishProgress("Finish",Integer.valueOf(numOfNewEvents).toString());
+			return numOfNewEvents;
+		}  
 		
-		private void addEventToDB(Event event) {
+		/**
+		 * Adds an event to the DB if it does not already exist.
+		 * 
+		 * @param event The event to be added
+		 * @return true if the event was added, false otherwise.
+		 */
+		private boolean addEventToDB(Event event) {
 			// Get our dao
 			RuntimeExceptionDao<Event, Integer> eventDao = getHelper().getEventDataDao();
-			// Store the event in the database
-			// Check if the event already exists
+			// Check if the event already exists in the DB
 			if(!isEventInDB(eventDao, event)){
+				// Store the event in the database
 				eventDao.create(event);
-			}
-		}
-
-		private boolean isEventInDB(RuntimeExceptionDao<Event, Integer> eventDao, Event event) {
-			//if(eventDao.queryForMatching(event) != null){
-			Map<String, Object> fieldValues = new HashMap<String,Object>();
-			fieldValues.put("name", event.getName());
-			fieldValues.put("day", event.getDay());
-			fieldValues.put("description", event.getDescription());
-			// eooo
-			if(eventDao.queryForFieldValuesArgs(fieldValues) != null){
-				System.out.println("El evento ya existe, no se añade.");
 				return true;
 			}
 			return false;
 		}
 
 		/**
-		 * If there was a problem, inform the user
+		 * Checks if an event is already in the DB. To determinate it, checks the name, day and description of an event.
+		 * 
+		 * @param eventDao Our DAO for the Events table
+		 * @param event The event to be checked its presence in the DB
+		 * @return true if the event is already in the DB
+		 */
+		private boolean isEventInDB(RuntimeExceptionDao<Event, Integer> eventDao, Event event) {
+			Map<String, Object> fieldValues = new HashMap<String,Object>();
+			fieldValues.put("name", event.getName());
+			fieldValues.put("day", event.getDay());
+			fieldValues.put("description", event.getDescription());
+			List<Event> foundEvents = eventDao.queryForFieldValuesArgs(fieldValues);
+			/*
+			 * for (int i=0; i<foundEvents.size();i++){
+				System.out.println(foundEvents.get(i).getName());
+			}
+			*/
+			if(foundEvents.size() >= 1){
+				System.out.println("El evento ya existe, no se añade.");
+				
+				return true;
+			}
+			return false;
+		}
+
+		/**
+		 * Informs the user of the state of the process of events download.
 		 */
 		protected void onProgressUpdate(String... progress) {
     		System.out.println("Estoy en onProgressUpdate:"+progress[0]);
-    		if (progress[0].equals("Exception")){
-    			Toast toast = Toast.makeText(context, "There were problems downloading the content from: " +progress[1] +" It's events won't be displayed.", Toast.LENGTH_LONG);
-    			toast.setGravity(Gravity.TOP, 0, FirstTimeActivity.actionBarHeight);
-		    	toast.show();
+    		if (progress[0].equals("Start")){
+    			displayToast(getString(R.string.searching));
+    		}else if (progress[0].equals("Exception")){
+    			displayToast("There were problems downloading the content from: " +progress[1] +" It's events won't be displayed.");
+    		}else if (progress[0].equals("Finish")){
+    			if (Integer.parseInt(progress[1]) > 0){
+    				displayToast("Added " +progress[1] +" new events");
+    			}else{
+    				displayToast(getString(R.string.no_new_events));
+    			}
     		}
     		//loadProgressBar.setProgress(progress[0].intValue());
 		}
@@ -374,18 +371,14 @@ public class FirstTimeActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 		/**
 		* Goes to the Date Activity and hides the progressBar.
         */
-		protected void onPostExecute(Map<String, List<Event>> result) {
-			System.out.println("onPostExecute de la primera tarea.");
+		protected void onPostExecute(Integer result) {
+			System.out.println("onPostExecute------------>");
 			loadProgressBar.setVisibility(View.GONE);
-			// Enable the button
-			loadButton.setEnabled(true);
-			// Go to the Date Activity
-			Intent intent = new Intent(context, DateActivity.class);
-			//intent.putParcelableArrayListExtra(EXTRA_HTML, (ArrayList<? extends Parcelable>) result);
-			//intent.putExtra(EXTRA_HTML, (Serializable)result);
-			//intent.putStringArrayListExtra(EXTRA_HTML, result);
-			startActivity(intent);
-			
+			// Reload the Date Activity if there are new events
+			if (result > 0){
+				Intent intent = new Intent(context, DateActivity.class);
+				startActivity(intent);
+			}
 		}
 	}
 }
