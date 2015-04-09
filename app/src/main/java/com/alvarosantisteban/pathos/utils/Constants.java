ec1b5813fbe7ccdf8ecdbc902e3d52cf4e63c212
Alvaro Santisteban <alvarosantisteban@gmail.com>
2015-04-09 12:43:57 +0200

    Create a Constants class.

---
+++ app/src/main/java/com/alvarosantisteban/pathos/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.alvarosantisteban.pathos.utils;
+
+/**
+ * Constants used all over the program
+ *
+ * @author Alvaro Santisteban Dieguez 9/04/15 - alvarosantisteban@gmail.com
+ */
+public final class Constants {
+
+    public static final String CHOSEN_DATE = "chosenDate";
+}
